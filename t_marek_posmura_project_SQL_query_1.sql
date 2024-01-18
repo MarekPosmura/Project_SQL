3bb8547bab104a9ff9f8d1cecf8014ccd291ce6c
Marek Posmura <m.posmura@seznam.cz>
2024-01-18 16:55:55 +0100

    draft of query number 4

--- t_marek_posmura_project_SQL_query_1.sql
+++ t_marek_posmura_project_SQL_query_1.sql
@@ -20,19 +20,3 @@ FROM (
 ) AS first_query
 WHERE previous_year IS NOT NULL
 -- AND percentage_diff < 0
-
-
--- předchozí varianta, zatím bez subselectu
-SELECT
-	tmp.industry_name,
-	tmp.avg_wage,
-	tmp2.avg_wage AS avg_wage_previous_year,
-	tmp.avg_wage - tmp2.avg_wage AS difference,
-	tmp.compared_year,
-	tmp2.compared_year AS previous_year
-FROM t_marek_posmura_project_sql_primary_finale tmp
-LEFT JOIN t_marek_posmura_project_sql_primary_finale tmp2
-	ON tmp.industry_name = tmp2.industry_name 
-	AND tmp.compared_year = tmp2.compared_year + 1 
-GROUP BY tmp.industry_name, tmp.compared_year 
-ORDER BY tmp.industry_name
